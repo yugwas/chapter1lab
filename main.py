044f4761778956336817ebc9dc60f2e0ed658be8
Kristine M <76263462+yugwas@users.noreply.github.com>
2021-09-09 11:14:54 +0000

    More formatting changes + made my code shorter.

--- main.py
+++ main.py
@@ -1,27 +1,18 @@
 import math
 
 # 1.1 Part A 
-fahrenheit = input("Enter temperature in Fahrenheit:  ")
-fahrenheit = float(fahrenheit)
+fahrenheit = float(input("Enter temperature in Fahrenheit:  "))
 celsius = (5/9) * (fahrenheit - 32)
 print("The temperature in Celsius:", round(celsius, 2), '\n')
 
-
 # 1.2 Part B
-height = input("Enter the height of the trapezoid:  ")
-bottom_base_length = input("Enter the length of the bottom base:  ")
-top_base_length = input("Enter the length of the top base:  ")
-height = float(height)
-bottom_base_length = float(bottom_base_length)
-top_base_length = float(top_base_length)
+height = float(input("Enter the height of the trapezoid:  "))
+bottom_base_length = float(input("Enter the length of the bottom base:  "))
+top_base_length = float(input("Enter the length of the top base:  "))
 area_of_trapezoid = (1/2) * (bottom_base_length + top_base_length) * (height)
 print("The area is:", round(area_of_trapezoid, 2), '\n')
 
-
 # 1.3 Part C
-radius = input("Enter the radius of the circle:  ")
-radius = float(radius)
+radius = float(input("Enter the radius of the circle:  "))
 area_of_circle = (math.pi) * (radius)**2
 print ("The area is:", round(area_of_circle, 2), '\n')
-
-# Mr. Heine said I got 9 points.
