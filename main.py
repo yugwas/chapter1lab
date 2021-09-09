75c4f2f0a8520c54692f81e9b148ec6b8de3c5be
Kristine M <76263462+yugwas@users.noreply.github.com>
2021-09-09 11:11:07 +0000

    Formatting changes. :)

--- main.py
+++ main.py
@@ -4,7 +4,8 @@ import math
 fahrenheit = input("Enter temperature in Fahrenheit:  ")
 fahrenheit = float(fahrenheit)
 celsius = (5/9) * (fahrenheit - 32)
-print("The temperature in Celsius:", celsius)
+print("The temperature in Celsius:", round(celsius, 2), '\n')
+
 
 # 1.2 Part B
 height = input("Enter the height of the trapezoid:  ")
@@ -14,12 +15,13 @@ height = float(height)
 bottom_base_length = float(bottom_base_length)
 top_base_length = float(top_base_length)
 area_of_trapezoid = (1/2) * (bottom_base_length + top_base_length) * (height)
-print("The area is:", area_of_trapezoid)
+print("The area is:", round(area_of_trapezoid, 2), '\n')
+
 
 # 1.3 Part C
 radius = input("Enter the radius of the circle:  ")
 radius = float(radius)
 area_of_circle = (math.pi) * (radius)**2
-print ("The area is:  ", area_of_circle)
+print ("The area is:", round(area_of_circle, 2), '\n')
 
 # Mr. Heine said I got 9 points.
